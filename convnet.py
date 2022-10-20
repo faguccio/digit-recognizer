86617db4c81fd78cf0ba435e73a37a7013500d85
Fabio <faga2703@gmail.com>
2022-10-20 11:27:41 +0200

    feature: added confusion matrix:

--- convnet.py
+++ convnet.py
@@ -7,6 +7,11 @@ from keras import backend as K
 import numpy as np
 
 
+
+
+
+
+
 def best_convnet(X_train, Y_train, X_val, Y_val):
     Y_train = to_categorical(Y_train)
     Y_val = to_categorical(Y_val)
@@ -23,14 +28,24 @@ def best_convnet(X_train, Y_train, X_val, Y_val):
 
     cnn.fit(
        X_train, Y_train,
-       #batch_size = 64,
-       epochs = 12,
+       batch_size = 32,
+       epochs = 3,
        verbose = 1,
        validation_data = (X_val, Y_val)
     )
         
-    score = cnn.evaluate(x_test, y_test, verbose = 0) 
+    score = cnn.evaluate(X_val, Y_val, verbose = 0) 
 
     print('Test loss:', score[0]) 
     print('Test accuracy:', score[1])
+
+    
+
+    from sklearn.metrics import confusion_matrix
+
+    y_pred = cnn.predict(X_val)
+    y_pred = [ list(a).index(max(a)) for a in y_pred]
+    Y_val = [ list(a).index(max(a)) for a in Y_val]
+    confusion_matrix = confusion_matrix(Y_val, np.rint(y_pred))
+    print(confusion_matrix)
 
